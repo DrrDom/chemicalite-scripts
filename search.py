f1e3b8c999dcae164f05b27bc01795d4b05c436a
guzelka <minibaeva07501@gmail.com>
2022-03-10 23:07:59 +0100

    changed output argument

--- search.py
+++ search.py
@@ -5,10 +5,9 @@ import sqlite3
 import os
 import pandas as pd
 from concurrent.futures import ProcessPoolExecutor
-from time import perf_counter
 from multiprocessing import cpu_count
 from functools import partial
-from itertools import islice, chain
+from itertools import islice
 from sim_search import sql_for_similarity
 
 
@@ -68,8 +67,6 @@ def main():
 
     args = parser.parse_args()
 
-    if os.path.isfile(args.output):
-        os.remove(args.output)
 
     df_mols = pd.read_csv(args.input_smiles, sep='\t')
     smiles = df_mols.smi.to_list()
@@ -77,16 +74,16 @@ def main():
     chunked = iter(partial(take, args.ncpu, zip(mol_ids, smiles)), []) # partial calls take function until output is an empty sheet
                                                                        # by take function islice extracts n elements from zip
                                                                        # with saving a condition about zip
-    start = perf_counter()
+    # start = perf_counter()
 
-    with open(args.output, 'a') as f, ProcessPoolExecutor(max_workers=args.ncpu) as p:
+    with open(args.output, 'wt') as f, ProcessPoolExecutor(max_workers=args.ncpu) as p:
         f.write('\t'.join(['smi', 'mol_id', 'chembl_smi', 'chembl_id', 'similarity']) + '\n')
-        for res in chain.from_iterable(p.map(
-                partial(calc_sim_for_smiles, db_name=args.input_db, fp=args.fp, mol_field=args.mol_field, table=args.table),
-                chunked)):
-            f.write('\t'.join(map(str, res)) + '\n')
+        for res in p.map(partial(calc_sim_for_smiles, db_name=args.input_db, fp=args.fp, mol_field=args.mol_field, table=args.table),
+                chunked):
+            for items in res:
+                f.write('\t'.join(map(str, items)) + '\n')
 
-    print(perf_counter() - start)
+    # print(perf_counter() - start)
 
 
 if __name__ == '__main__':
