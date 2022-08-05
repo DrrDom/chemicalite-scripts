bf700bf9ebea5db0910b169d3bbc5e29f4c048e3
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-08-05 15:35:39 +0200

    Add support of custom radius of Morgan fingerprint

--- add_fp_to_db.py
+++ add_fp_to_db.py
@@ -4,6 +4,14 @@ import argparse
 import sqlite3
 
 
+def compose_index_table_name(main_table_name, fp, radius_morgan):
+    if fp == 'morgan':
+        index_table_name = f'{main_table_name}_{fp}{radius_morgan}_idx'
+    else:
+        index_table_name = f'{main_table_name}_{fp}_idx'
+    return index_table_name
+
+
 def main():
     parser = argparse.ArgumentParser(description='Similarity search in SQLite DB.')
     parser.add_argument('-d', '--input_db', metavar='FILENAME', required=True,
@@ -14,6 +22,8 @@ def main():
                         help='column which contains Mol object. Default: mol.')
     parser.add_argument('-f', '--fp', metavar='STRING', default='morgan', choices=['morgan', 'pattern', 'atom_pairs'],
                         help='fingerprint type to compute. Default: morgan.')
+    parser.add_argument('-r', '--radius_morgan', metavar='INTEGER', default=2, type=int,
+                        help='radius of Morgan fingerprint. Default: 2.')
 
     args = parser.parse_args()
     with sqlite3.connect(args.input_db) as con:
@@ -24,12 +34,13 @@ def main():
 
         # create a virtual table to be filled with bfp data
         # con.execute(f"DROP TABLE IF EXISTS {args.table}_{args.fp}_idx")
-        con.execute(f"CREATE VIRTUAL TABLE IF NOT EXISTS {args.table}_{args.fp}_idx "
+        index_table_name = compose_index_table_name(args.table, args.fp, args.radius_morgan)
+        con.execute(f"CREATE VIRTUAL TABLE IF NOT EXISTS {index_table_name} "
                     f"USING rdtree(id, fp bits(2048))")
 
         # compute and insert the fingerprints
-        sql = f"INSERT INTO {args.table}_{args.fp}_idx(id, fp) " \
-              f"SELECT rowid, mol_{args.fp}_bfp({args.mol_field}, {'2,' if args.fp == 'morgan' else ''} 2048) " \
+        sql = f"INSERT INTO {index_table_name}(id, fp) " \
+              f"SELECT rowid, mol_{args.fp}_bfp({args.mol_field}, {f'{args.radius_morgan},' if args.fp == 'morgan' else ''} 2048) " \
               f"FROM {args.table} " \
               f"WHERE {args.mol_field} IS NOT NULL"
         con.execute(sql)
--- sim_search.py
+++ sim_search.py
@@ -4,18 +4,21 @@ import argparse
 import sqlite3
 import sys
 
+from add_fp_to_db import compose_index_table_name
 
-def sql_for_similarity(fp, mol_field, table, limit=None):
+
+def sql_for_similarity(fp, mol_field, table, limit=None, radius_morgan=2):
+    index_table_name = compose_index_table_name(main_table_name=table, fp=fp, radius_morgan=radius_morgan)
     sql = f"""SELECT 
                     main.smi, 
                     main.id, 
-                    bfp_tanimoto(mol_{fp}_bfp(main.{mol_field}, {'2,' if fp == 'morgan' else ''} 2048), 
-                                 mol_{fp}_bfp(mol_from_smiles(?1), {'2,' if fp == 'morgan' else ''} 2048)) as t 
+                    bfp_tanimoto(mol_{fp}_bfp(main.{mol_field}, {f'{radius_morgan},' if fp == 'morgan' else ''} 2048), 
+                                 mol_{fp}_bfp(mol_from_smiles(?1), {f'{radius_morgan},' if fp == 'morgan' else ''} 2048)) as t 
                   FROM 
-                    {table} AS main, {table}_{fp}_idx AS idx
+                    {table} AS main, {index_table_name} AS idx
                   WHERE 
                     main.rowid = idx.id AND
-                    idx.id MATCH rdtree_tanimoto(mol_{fp}_bfp(mol_from_smiles(?1), {'2,' if fp == 'morgan' else ''} 2048), ?2) 
+                    idx.id MATCH rdtree_tanimoto(mol_{fp}_bfp(mol_from_smiles(?1), {f'{radius_morgan},' if fp == 'morgan' else ''} 2048), ?2) 
                   ORDER BY t DESC 
                   {'LIMIT ' + str(limit) if limit is not None else ''}"""
     return sql
@@ -36,6 +39,8 @@ def main():
                         help='field name where mol objects are stored. Default: mol.')
     parser.add_argument('-f', '--fp', metavar='STRING', default='morgan', choices=['morgan', 'pattern', 'atom_pairs'],
                         help='fingerprint type to compute. Default: morgan.')
+    parser.add_argument('-r', '--radius_morgan', metavar='INTEGER', default=2, type=int,
+                        help='radius of Morgan fingerprint. Default: 2.')
     parser.add_argument('-p', '--threshold', metavar='NUMERIC', default=0.7, type=float,
                         help='Tanimoto similarity threshold. Default: 0.7.')
     parser.add_argument('-l', '--limit', metavar='INTEGER', default=None, type=int,
@@ -48,7 +53,7 @@ def main():
         con.load_extension('chemicalite')
         con.enable_load_extension(False)
 
-        sql = sql_for_similarity(args.fp, args.mol_field, args.table, args.limit)
+        sql = sql_for_similarity(args.fp, args.mol_field, args.table, args.limit, args.radius_morgan)
 
         res = con.execute(sql, (args.query, args.threshold)).fetchall()
 
